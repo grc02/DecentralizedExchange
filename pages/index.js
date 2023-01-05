8ef2fd2fccc0df1f8d510ce0ebe539d281c9b985
grc02 <georgi.chonkov27@gmail.com>
2023-01-05 17:08:38 +0200

    Added a new dir with all .png files and made minor changes to the files in the 'pages/' dir

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,7 @@
-export default function Home() {
-  return <div className="home">Home</div>;
-}
+import React from "react";
+
+const Home = () => {
+  return <div>Home</div>;
+};
+
+export default Home;
