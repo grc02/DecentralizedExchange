befb2ba0356dbd4ab89545e582572d5e6fe392ae
grc02 <georgi.chonkov27@gmail.com>
2023-01-07 23:34:07 +0200

    Added the left part of the navbar wich comprises of the logo of the dex and links to diff pages

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Style from "./Navbar.module.css";
+import images from "../../assets";
+import { Model, TokenList } from "../index";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const menuItems = [
+    {
+      name: "Swap",
+      link: "/",
+    },
+    {
+      name: "Tokens",
+      link: "/",
+    },
+    {
+      name: "Pools",
+      link: "/",
+    },
+  ];
+  const [openModel, setOpenModel] = useState(false);
+  const [openTokenBox, setOpenTokenBox] = useState(false);
+
+  return (
+    <div className={Style.Navbar}>
+      <div className={Style.NavbarBox}>
+        <div className={Style.NavbarBoxLeft}>
+          {/* // Logo Image */}
+          <div className={Style.NavbarBoxLeftImage}>
+            <Image src={images.uniswap} alt="logo" width={50} height={50} />
+          </div>
+          {/* // Menu */}
+          <div className={Style.NavbarBoxLeftMenu}>
+            {menuItems.map((el, i) => (
+              <Link key={i + 1} href={{ pathname: `${el.name}` }}>
+                <p className={Style.NavbarBoxLeftMenuItem}>{el.name}</p>
+              </Link>
+            ))}
+          </div>
+        </div>
+        <div className={Style.NavbarBoxRight}>Right</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
