52047722265d381ef92b37bfb8762b11f5abb0c1
grc02 <georgi.chonkov27@gmail.com>
2023-01-08 20:26:59 +0200

    Created the Model component, which allows the users to connect their wallet

--- components/Model/Model.jsx
+++ components/Model/Model.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
 
-const Model = () => {
-  return <div>Model</div>;
+import Style from "./Model.module.css";
+import images from "../../assets";
+
+const Model = ({ setOpenModel, connectWallet }) => {
+  const walletMenu = ["Metamask", "Coinbase", "Wallet", "WalletConnect"];
+
+  return (
+    <div className={Style.Model}>
+      <div className={Style.ModelBox}>
+        <div className={Style.ModelBoxHeader}>
+          <p>Connect wallet</p>
+          <div className={Style.ModelBoxHeaderImage}>
+            <Image
+              src={images.close}
+              alt="logo"
+              width={50}
+              height={50}
+              onClick={() => setOpenModel(false)}
+            ></Image>
+          </div>
+        </div>
+
+        <div className={Style.ModelBoxWallet}>
+          {walletMenu.map((el, i) => (
+            <p key={i + 1} onClick={() => connectWallet()}>
+              {el}
+            </p>
+          ))}
+        </div>
+        <p className={Style.ModelBoxParagraph}>
+          By connecting a wallet, you agree to Uniswap Labs’
+          <br /> Terms of Service and consent to its Privacy Policy.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Model;
