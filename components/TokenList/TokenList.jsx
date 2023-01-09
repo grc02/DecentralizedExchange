65158b86d4adc42b9a9af958349b86d927327304
grc02 <georgi.chonkov27@gmail.com>
2023-01-09 14:01:19 +0200

    Created the 'TokenList' component which displays all the tokens that a particular account owns

--- components/TokenList/TokenList.jsx
+++ components/TokenList/TokenList.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
 
-const TokenList = () => {
-  return <div>TokenList</div>;
+import Style from "./TokenList.module.css";
+import images from "../../assets";
+
+const TokenList = ({ setOpenTokenBox, tokenData }) => {
+  const data = [1, 2, 3, 4, 5, 6];
+  return (
+    <div className={Style.TokenList}>
+      <p
+        className={Style.TokenListClose}
+        onClick={() => {
+          setOpenTokenBox(false);
+        }}
+      >
+        <Image src={images.close} alt="logo" width={50} height={50} />
+      </p>
+      <div className={Style.TokenListTitle}>Your Token List</div>
+
+      {data.map((el, i) => (
+        <div className={Style.TokenListBox}>
+          <div className={Style.TokenListBoxInfo}>
+            <p className={Style.TokenListBoxInfoSymbol}>SYMBOL</p>
+            <p>
+              <span>27</span> TOKEN
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TokenList;
