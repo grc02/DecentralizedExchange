929a63ad218f1a15a24ccd8b3357ea091d64305c
grc02 <georgi.chonkov27@gmail.com>
2023-01-10 00:50:49 +0200

    Added a new component, which allows the user to choose the prefered settings

---
+++ components/Token/Token.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import Image from "next/image";
+
+import Style from "./Token.module.css";
+import images from "../../assets";
+import { Toggle } from "../index";
+
+const Token = ({ setOpenSetting }) => {
+  return (
+    <div className={Style.Token}>
+      <div className={Style.TokenBox}>
+        <div className={Style.TokenBoxHeader}>
+          <h2>Interface Settings</h2>
+          <Image
+            src={images.close}
+            alt="close"
+            width={50}
+            height={50}
+            onClick={() => setOpenSetting(false)}
+          />
+        </div>
+        <p className={Style.TokenBoxParagraph}>
+          Slippage tolerance
+          <Image src={images.question} alt="lock" width={30} height={30} />
+        </p>
+
+        <div className={Style.TokenBoxInput}>
+          <button>Auto</button>
+          <input type="text" placeholder="0.1%" />
+        </div>
+
+        <p className={Style.TokenBoxParagraph}>
+          Transaction deadline
+          <Image src={images.question} alt="lock" width={30} height={30} />
+        </p>
+
+        <div className={Style.TokenBoxInput}>
+          <button>Minutes</button>
+          <input type="text" placeholder="10" />
+        </div>
+
+        <br />
+
+        <div className={Style.TokenBoxToggle}>
+          <Toggle />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Token;
