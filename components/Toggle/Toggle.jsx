80672af26af65260b937ebda1509d0059ab54fb5
grc02 <georgi.chonkov27@gmail.com>
2023-01-10 22:31:58 +0200

    Added a 'Toggle' component, which the user can select whether they want to have a deadline or not

--- components/Toggle/Toggle.jsx
+++ components/Toggle/Toggle.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
-const Toggle = () => {
-  return <div>Toggle</div>;
+import Style from "./Toggle.module.css";
+
+const Toggle = ({ label }) => {
+  return (
+    <div className={Style.Toggle}>
+      <div className={Style.ToggleSwitch}>
+        <input
+          type="checkbox"
+          className={Style.ToggleCheckbox}
+          name={label}
+          id={label}
+        />
+        <label className={Style.ToggleLabel} htmlFor={label}>
+          <span className={Style.ToggleInner} />
+          <span className={Style.ToggleSwitch} />
+        </label>
+      </div>
+    </div>
+  );
 };
 
 export default Toggle;
