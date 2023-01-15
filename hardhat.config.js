f5196c8398fd3e184b3dd1683e2a7125c13bf81e
grc02 <georgi.chonkov27@gmail.com>
2023-01-15 18:18:30 +0200

    Installed 'dotenv' package for managing the private keys and rpc urls

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: MAINNET_RPC_URL,
+      },
+    },
+  },
 };
