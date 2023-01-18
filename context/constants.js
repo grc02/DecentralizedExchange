dbe6b1d6a76b2e033e15feeb4b040ca2dde876d6
grc02 <georgi.chonkov27@gmail.com>
2023-01-18 15:16:22 +0200

    Created a new 'context' dir, where the abis of all contracts are exported and their addresses, when deployed to a local node

---
+++ context/constants.js
@@ -0,0 +1,21 @@
+import One from "./json/One.json";
+import Two from "./json/Two.json";
+import SingleSwapToken from "./json/SingleSwapToken.json";
+import SwapMultiHop from "./json/SwapMultiHop.json";
+import IWETH from "./json/IWETH.json";
+
+export const OneTokenAddress = "0x09120eAED8e4cD86D85a616680151DAA653880F2";
+export const OneTokenABI = One.abi;
+
+export const TwoTokenAddress = "0x3E661784267F128e5f706De17Fac1Fc1c9d56f30";
+export const TwoTokenABI = Two.abi;
+
+export const SingleSwapTokenAddress =
+  "0x1D3EDBa836caB11C26A186873abf0fFeB8bbaE63";
+export const SingleSwapTokenABI = SingleSwapToken.abi;
+
+export const SwapMultiHopAddress = "0x6732128F9cc0c4344b2d4DC6285BCd516b7E59E6";
+export const SwapMultiHopABI = SwapMultiHop.abi;
+
+export const IWETHAddress = "0x15Ff10fCc8A1a50bFbE07847A22664801eA79E0f";
+export const IWETHABI = IWETH.abi;
