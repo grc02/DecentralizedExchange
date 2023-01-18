3209959248fc354c0ae46d6f5b73d91d59548f57
grc02 <georgi.chonkov27@gmail.com>
2023-01-18 15:06:06 +0200

    Added a simple script, which deploys all contracts to the local node

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,10 +1,34 @@
-const ethers = require("hardhat");
+const hre = require("hardhat");
 
 async function main() {
-  const Contract = await ethers.getContractFactory("...");
-  const contract = await Contract.deploy();
+  //Deploying Demo Tokens
+  const One = await hre.ethers.getContractFactory("One");
+  const one = await One.deploy();
 
-  await contract.deployed();
+  await one.deployed();
+  console.log(`One Token deployed at: ${one.address}`);
+
+  const Two = await hre.ethers.getContractFactory("Two");
+  const two = await Two.deploy();
+
+  await two.deployed();
+  console.log(`Two Token deployed at: ${two.address}`);
+
+  //SingleSwapToken
+  const SingleSwapToken = await hre.ethers.getContractFactory(
+    "SingleSwapToken"
+  );
+  const singleSwapToken = await SingleSwapToken.deploy();
+
+  await singleSwapToken.deployed();
+  console.log(`SingleSwapToken deployed at: ${singleSwapToken.address}`);
+
+  //SwapMultiHop
+  const SwapMultiHop = await hre.ethers.getContractFactory("SwapMultiHop");
+  const swapMultiHop = await SwapMultiHop.deploy();
+
+  await swapMultiHop.deployed();
+  console.log(`SwapMultiHop deployed at: ${swapMultiHop.address}`);
 }
 
 main().catch((error) => {
