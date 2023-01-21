10eeefc9af43683836922f57dbcf652f50fae9a1
grc02 <georgi.chonkov27@gmail.com>
2023-01-21 23:30:14 +0200

    Changed contracts addresses, depending on which network we are connected

--- context/constants.js
+++ context/constants.js
@@ -4,17 +4,29 @@ import SingleSwapToken from "./json/SingleSwapToken.json";
 import SwapMultiHop from "./json/SwapMultiHop.json";
 import IWETH from "./json/IWETH.json";
 
-export const OneTokenAddress = "0x09120eAED8e4cD86D85a616680151DAA653880F2";
+// MAINNET FORK:
+// One Token deployed at: 0xbe18A1B61ceaF59aEB6A9bC81AB4FB87D56Ba167
+// Two Token deployed at: 0x25C0a2F0A077F537Bd11897F04946794c2f6f1Ef
+// SingleSwapToken deployed at: 0x01cf58e264d7578D4C67022c58A24CbC4C4a304E
+// SwapMultiHop deployed at: 0xd038A2EE73b64F30d65802Ad188F27921656f28F
+
+// LOCAL NETWORK:
+// One Token deployed at: 0x5FbDB2315678afecb367f032d93F642f64180aa3
+// Two Token deployed at: 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
+// SingleSwapToken deployed at: 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0
+// SwapMultiHop deployed at: 0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9
+
+export const OneTokenAddress = "0xbe18A1B61ceaF59aEB6A9bC81AB4FB87D56Ba167";
 export const OneTokenABI = One.abi;
 
-export const TwoTokenAddress = "0x3E661784267F128e5f706De17Fac1Fc1c9d56f30";
+export const TwoTokenAddress = "0x25C0a2F0A077F537Bd11897F04946794c2f6f1Ef";
 export const TwoTokenABI = Two.abi;
 
 export const SingleSwapTokenAddress =
-  "0x1D3EDBa836caB11C26A186873abf0fFeB8bbaE63";
+  "0x01cf58e264d7578D4C67022c58A24CbC4C4a304E";
 export const SingleSwapTokenABI = SingleSwapToken.abi;
 
-export const SwapMultiHopAddress = "0x6732128F9cc0c4344b2d4DC6285BCd516b7E59E6";
+export const SwapMultiHopAddress = "0xd038A2EE73b64F30d65802Ad188F27921656f28F";
 export const SwapMultiHopABI = SwapMultiHop.abi;
 
 export const IWETHAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
