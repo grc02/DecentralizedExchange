a02e777779fc493665c69e9c114a1c0b689bce49
grc02 <georgi.chonkov27@gmail.com>
2023-01-27 23:13:22 +0200

    Created the 'Liquidity' contract and set it up

---
+++ contracts/Liquidity.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.7.6 <0.9.0;
+pragma abicoder v2;
+
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/INonfungiblePositionManager.sol";
+import "@uniswap/v3-periphery/contracts/base/LiquidityManagement.sol";
+import "hardhat/console.sol";
+
+contract LiquidityExamples is IERC721Receiver {
+    address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
+
+    // 0.01% fee
+    uint24 public constant POOL_FEE = 100;
+
+    INonfungiblePositionManager public nonfungiblePositionManager =
+        INonfungiblePositionManager(0xC36442b4a4522E871399CD717aBDD847Ab11FE88);
+
+    /// @notice Represents the deposit of an NFT
+    struct Deposit {
+        address owner;
+        uint128 liquidity;
+        address token0;
+        address token1;
+    }
+
+    /// @dev deposits[tokenId] => Deposit
+    mapping(uint => Deposit) public deposits;
+
+    // Store token id used in this example
+    uint public tokenId;
+}
