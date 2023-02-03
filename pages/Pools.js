0212bba30986bf606185e78cfddaff6c976b2699
grc02 <georgi.chonkov27@gmail.com>
2023-02-03 11:38:04 +0200

    Added the last page of the dex, where the user can connect his/her wallet and add liquidity

---
+++ pages/Pools.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Image from "next/image";
+
+import images from "../assets";
+import Style from "../styles/Pools.module.css";
+
+import { PoolAdd, PoolConnect } from "../components/index";
+
+const Pool = () => {
+  return (
+    <div className={Style.Pool}>
+      <PoolAdd />
+    </div>
+  );
+};
+
+export default Pool;
