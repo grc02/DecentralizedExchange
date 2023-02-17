f3cad4a9dacc9f6726149d41f76ef47d0411d75a
grc02 <georgi.chonkov27@gmail.com>
2023-02-17 22:17:44 +0200

    Refactored some portion of the 'HeroSection' component with dynamic data

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -7,30 +7,67 @@ import { Token, SearchToken } from "../index";
 
 import { SwapTokenContext } from "../../context/swapContext";
 
-const HeroSection = ({ tokenData }) => {
+const HeroSection = () => {
   const [openSetting, setOpenSetting] = useState(false);
   const [openToken, setOpenToken] = useState(false);
   const [openTokensTwo, setOpenTokensTwo] = useState(false);
 
-  const { singleSwapToken, connectWallet, account, ether, dai } =
-    useContext(SwapTokenContext);
-  // ether,
-  // dai,
-  // tokenData,
-  // getPrice,
-  // swapUpdatePrice
+  const [tokenSwapOutput, setTokenSwapOutput] = useState(0);
+  const [poolMessage, setPoolMessage] = useState("");
+  const [search, setSearch] = useState(false);
+  const [swapAmount, setSwapAmount] = useState(0);
+
+  const {
+    singleSwapToken,
+    connectWallet,
+    account,
+    ether,
+    dai,
+    tokenData,
+    getPrice,
+    swapUpdatePrice,
+  } = useContext(SwapTokenContext);
 
   //Token 1 details
   const [tokenOne, setTokenOne] = useState({
     name: "",
     image: "",
+    symbol: "",
+    tokenBalance: "",
+    tokenAddress: "",
   });
 
   //Token 2 details
   const [tokenTwo, setTokenTwo] = useState({
     name: "",
     image: "",
+    symbol: "",
+    tokenBalance: "",
+    tokenAddress: "",
   });
+
+  const callOutput = async (value) => {
+    const yourAccount = "0x97f991971a37D4Ca58064e6a98FC563F03A71E5c";
+    const deadline = 10;
+    const slippageAmount = 25;
+    const data = await swapUpdatePrice(
+      value,
+      slippageAmount,
+      deadline,
+      yourAccount
+    );
+    console.log(data);
+
+    setTokenSwapOutput(data[1]);
+    setSearch(false);
+
+    const poolAddress = "0xc2e9f25be6257c210d7adf0d4cd6e3e881ba25f8";
+    const poolData = await getPrice(value, poolAddress);
+    const message = `${value} ${poolData[2]} = ${poolData[0]} ${poolData[1]}`;
+    console.log(message);
+    setPoolMessage(message);
+  };
+
   return (
     <div className={Style.HeroSection}>
       <div className={Style.HeroSectionBox}>
@@ -49,7 +86,15 @@ const HeroSection = ({ tokenData }) => {
 
         {/*// TOKEN 1 */}
         <div className={Style.HeroSectionBoxInput}>
-          <input type="text" placeholder="0" />
+          <input
+            type="text"
+            placeholder="0"
+            onChange={(e) => (
+              callOutput(e.target.value),
+              setSwapAmount(e.target.value),
+              setSearch(true)
+            )}
+          />
           <button onClick={() => setOpenToken(true)}>
             <Image
               src={tokenOne.image || images.etherlogo}
@@ -57,14 +102,26 @@ const HeroSection = ({ tokenData }) => {
               width={20}
               height={20}
             ></Image>
-            {tokenOne.name || "ETH"}
-            <small>{ether.slice(0, 7)}</small>
+            {tokenOne.symbol || "ETH"}
+            <small>{tokenOne.tokenBalance.slice(0, 7)}</small>
           </button>
         </div>
 
         {/*// TOKEN 2 */}
         <div className={Style.HeroSectionBoxInput}>
-          <input type="text" placeholder="0" />
+          {/* <input type="text" placeholder="0" /> */}
+          <p>
+            {search ? (
+              <Image
+                src={images.loading}
+                width={100}
+                height={40}
+                alt="loading"
+              />
+            ) : (
+              tokenSwapOutput
+            )}
+          </p>
           <button onClick={() => setOpenTokensTwo(true)}>
             <Image
               src={tokenTwo.image || images.etherlogo}
@@ -72,10 +129,16 @@ const HeroSection = ({ tokenData }) => {
               width={20}
               height={20}
             ></Image>
-            {tokenTwo.name || "ETH"}
-            <small>{dai.slice(0, 7)}</small>
+            {tokenTwo.symbol || "ETH"}
+            <small>{tokenTwo.tokenBalance.slice(0, 7)}</small>
           </button>
         </div>
+
+        {search ? (
+          <Image src={images.loading} width={100} height={40} alt="loading" />
+        ) : (
+          poolMessage
+        )}
 
         {account ? (
           <button
