5f72378c6c6ef7b1b2eb39b09edaaa6184dcf164
grc02 <georgi.chonkov27@gmail.com>
2023-02-19 18:24:42 +0200

    Made changes to the config file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,28 +1,25 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
 const MAINNET_RPC_URL = process.env.NEXT_PUBLIC_MAINNET_RPC_URL || "";
+const PRIVATE_KEY = process.env.NEXT_PUBLIC_PRIVATE_KEY || "0x";
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: {
-    compilers: [
-      {
-        version: "0.7.6",
-        settings: {
-          evmVersion: "istanbul",
-          optimizer: {
-            enabled: true,
-            runs: 1000,
-          },
-        },
+    version: "0.7.6",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 5000,
+        details: { yul: false },
       },
-    ],
+    },
   },
   networks: {
     hardhat: {
       forking: {
         url: MAINNET_RPC_URL,
+        accounts: [`0x${PRIVATE_KEY}`],
       },
     },
   },
