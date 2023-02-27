f710869a93246c4c4ba31d63a35c83620cfa2302
grc02 <georgi.chonkov27@gmail.com>
2023-02-27 12:03:07 +0200

    Added main functionality so the user can add liquidity through the UI

--- utils/addLiquidity.js
+++ utils/addLiquidity.js
@@ -33,3 +33,134 @@ async function getPoolData(poolContract) {
     tick: slot0[1],
   };
 }
+
+export const addLiquidityExternal = async (
+  tokenAddress1,
+  tokenAddress2,
+  poolAddress,
+  poolFee,
+  tokenAmount1,
+  tokenAmount2
+) => {
+  const web3modal = new Web3Modal();
+  const connection = await web3modal.connect();
+  const provider = new ethers.providers.Web3Provider(connection);
+  const signer = provider.getSigner();
+  const accountAddress = await signer.getAddress();
+
+  const token1Contract = new Contract(
+    tokenAddress1,
+    artifacts.WETH9.abi,
+    provider
+  );
+  const token2Contract = new Contract(
+    tokenAddress2,
+    artifacts.WETH9.abi,
+    provider
+  );
+
+  await token1Contract
+    .connect(signer)
+    .approve(
+      positionManagerAddress,
+      ethers.utils.parseEther(tokenAmount1.toString())
+    );
+
+  await token2Contract
+    .connect(signer)
+    .approve(
+      positionManagerAddress,
+      ethers.utils.parseEther(tokenAmount2.toString())
+    );
+
+  const poolContract = new Contract(
+    poolAddress,
+    artifacts.UniswapV3Pool.abi,
+    provider
+  );
+
+  const { chainId } = await provider.getNetwork();
+
+  //TOKEN1
+  const token1Name = await token1Contract.name();
+  const token1Symbol = await token1Contract.symbol();
+  const token1Decimals = await token1Contract.decimals();
+  const token1Address = token1Contract.address;
+
+  //TOKEN2
+  const token2Name = await token2Contract.name();
+  const token2Symbol = await token2Contract.symbol();
+  const token2Decimals = await token2Contract.decimals();
+  const token2Address = token2Contract.address;
+
+  const TokenA = new Token(
+    chainId,
+    token1Address,
+    token1Decimals,
+    token1Name,
+    token1Symbol
+  );
+  const TokenB = new Token(
+    chainId,
+    token2Address,
+    token2Decimals,
+    token2Name,
+    token2Symbol
+  );
+
+  const poolData = await getPoolData(poolContract);
+  console.log(poolData);
+
+  const pool = new Pool(
+    TokenA,
+    TokenB,
+    poolData.fee,
+    poolData.sqrtPriceX96.toString(),
+    poolData.liquidity.toString(),
+    poolData.tick
+  );
+
+  const position = new Position({
+    pool: pool,
+    liquidity: ethers.utils.parseUnits("1"),
+    tickLower:
+      nearestUsableTick(poolData.tick, poolData.tickSpacing) -
+      poolData.tickSpacing * 2,
+    tickUpper:
+      nearestUsableTick(poolData.tick, poolData.tickSpacing) +
+      poolData.tickSpacing * 2,
+  });
+
+  const { amount0: amount0Desired, amount1: amount1Desired } =
+    position.mintAmounts;
+
+  const params = {
+    token0: tokenAddress1,
+    token1: tokenAddress2,
+    fee: poolData.fee,
+    tickLower:
+      nearestUsableTick(poolData.tick, poolData.tickSpacing) -
+      poolData.tickSpacing * 2,
+    tickUpper:
+      nearestUsableTick(poolData.tick, poolData.tickSpacing) +
+      poolData.tickSpacing * 2,
+    amount0Desired: amount0Desired.toString(),
+    amount1Desired: amount1Desired.toString(),
+    amount0Min: 0,
+    amount1Min: 0,
+    recipient: accountAddress,
+    deadline: Math.floor(Date.now() / 1000) + 60 * 10,
+  };
+
+  const nonfungiblePositionManager = new Contract(
+    positionManagerAddress,
+    artifacts.NonfungiblePositionManager.abi,
+    provider
+  );
+
+  const tx = await nonfungiblePositionManager.connect(signer).mint(params, {
+    gasLimit: "1000000",
+  });
+  const receipt = await tx.wait();
+  return receipt;
+};
