ea06b96df0f2ddcfd9d99f63b061232274a281f3
grc02 <georgi.chonkov27@gmail.com>
2023-03-06 23:42:03 +0200

    Modified the 'Pools' page in order to be more responsive and have dynamic data

--- pages/Pools.js
+++ pages/Pools.js
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
 import Image from "next/image";
 
 import images from "../assets";
 import Style from "../styles/Pools.module.css";
 
 import { PoolAdd, PoolConnect } from "../components/index";
+import { SwapTokenContext } from "../context/swapContext";
 
 const Pool = () => {
+  const { account, createLiquidityAndPool, tokenData, getAllLiquidity } =
+    useContext(SwapTokenContext);
+  // Show the pool data only when the user is connected
+  const [closePool, setClosePool] = useState(false);
+
   return (
     <div className={Style.Pool}>
-      <PoolAdd />
+      {" "}
+      {account ? (
+        <PoolAdd
+          account={account}
+          setClosePool={setClosePool}
+          tokenData={tokenData}
+          createLiquidityAndPool={createLiquidityAndPool}
+        />
+      ) : (
+        <PoolConnect
+          account={account}
+          setClosePool={setClosePool}
+          getAllLiquidity={getAllLiquidity}
+        />
+      )}
     </div>
   );
 };
