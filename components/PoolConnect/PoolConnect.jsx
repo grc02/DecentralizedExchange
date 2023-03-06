7d971139af5f9080217e591389770d4bc14685af
grc02 <georgi.chonkov27@gmail.com>
2023-03-07 00:46:26 +0200

    Refactored the code so that dynamic data and different options can be displayed, when an user is connected with their account, otherwise give them the option to connect with their wallet

--- components/PoolConnect/PoolConnect.jsx
+++ components/PoolConnect/PoolConnect.jsx
@@ -4,20 +4,73 @@ import Image from "next/image";
 import Style from "./PoolConnect.module.css";
 import images from "../../assets";
 
-const PoolConnect = () => {
+const PoolConnect = ({ account, setClosePool, getAllLiquidity }) => {
+  let tokenList = [];
+  for (let i = 0; i < getAllLiquidity.length / 2; i++) {
+    tokenList.push(getAllLiquidity[i]);
+  }
+
   return (
     <div className={Style.PoolConnect}>
       <div className={Style.PoolConnectBox}>
         <div className={Style.PoolConnectBoxHeader}>
           <h2>Pool</h2>
-          <p>+ New Position</p>
+          <p onClick={() => setClosePool(true)}>+ New Position</p>
         </div>
 
-        <div className={Style.PoolConnectBoxMiddle}>
-          <Image src={images.wallet} alt="wallet" height={80} width={80} />
-          <p>Your active V3 liquidity positions will appear here</p>
-          <button>Connect Wallet</button>
-        </div>
+        {account ? (
+          <div className={Style.PoolConnectBoxMiddle}>
+            <Image src={images.wallet} alt="wallet" height={80} width={80} />
+            <p>Your active V3 liquidity positions will appear here</p>
+            <button>Connect Wallet</button>
+          </div>
+        ) : (
+          <div className={Style.PoolConnectBoxLiquidity}>
+            <div className={Style.PoolConnectBoxLiquidityHeader}>
+              <p>Your Position {tokenList.length}</p>
+            </div>
+
+            {tokenList.map((el, i) => (
+              <div className={Style.PoolConnectBoxLiquidityBox}>
+                <div className={Style.PoolConnectBoxLiquidityList}>
+                  <p>
+                    <small className={Style.mark}>
+                      {el.poolExample.token0.name}
+                    </small>
+                    {""}
+                    <small className={Style.mark}>
+                      {el.poolExample.token1.name}
+                    </small>
+                    {""}
+                    <span className={(Style.paragraph, Style.hide)}>
+                      {el.poolExample.token0.name} /{el.poolExample.token1.name}
+                    </span>
+                    {""}
+                    <span className={Style.paragraph}>{el.fee}</span>
+                    {""}
+                  </p>
+                  <p className={Style.highlight}>In Range</p>
+                </div>
+                <div className={Style.PoolConnectBoxLiquidityListInfo}>
+                  <p>
+                    <small>Min: 0.999</small>
+                    {""}
+                    <span>
+                      {el.poolExample.token0.name} per {""}
+                      {el.poolExample.token1.name}
+                    </span>
+                    {""} <span>--------</span> <small>Max: 1.000</small>
+                    {""}
+                    <span className={Style.hide}>
+                      {el.poolExample.token0.name} per {""}{" "}
+                      {el.poolExample.token1.name}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
 
         <div className={Style.PoolConnectBoxInfo}>
           <div className={Style.PoolConnectBoxInfoLeft}>
